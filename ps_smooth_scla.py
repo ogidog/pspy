c85c9f2ea2f05ccc3060b1f0c98e9cc66eddf72c
ogidog <ogidog@mail.ru>
2020-04-06 13:47:06 +0700

    Home commit

--- ps_smooth_scla.py
+++ ps_smooth_scla.py
@@ -1,5 +1,11 @@
-import sys
+import os, sys, platform
+
+from collections import defaultdict
+from itertools import permutations
+
 from scipy.io import loadmat, savemat
+from scipy.spatial import Delaunay
+
 from getparm import get_parm_value as getparm
 
 
@@ -9,7 +15,7 @@ def ps_smooth_scla(use_small_baselines):
     scn_wavelength = 100;
     small_baseline_flag = getparm('small_baseline_flag')
 
-    psver = loadmat('psver.mat')
+    psver = loadmat('psver.mat')['psver'][0][0]
     psname = 'ps' + str(psver)
     bpname = 'bp' + str(psver)
 
@@ -29,3 +35,28 @@ def ps_smooth_scla(use_small_baselines):
     C_ps_uw = scla['C_ps_uw']
     ph_ramp = scla['ph_ramp']
     scla.clear()
+
+    n_ps = ps['n_ps'][0][0]
+    n_ifg = ps['n_ifg'][0][0]
+
+    print('Number of points per ifg: {}'.format(n_ps))
+
+    arch = platform.platform().split('-')[0].lower()
+    if arch == 'windows':
+        use_triangle = 'n'
+    else:
+        tripath = os.system('which triangle > /dev/null');
+        if tripath == 0:
+            use_triangle = 'y'
+        else:
+            use_triangle = 'n';
+
+    if use_triangle == 'y':
+        nodename = 'scla.1.node'
+    else:
+        xy = ps['xy']
+        tri = Delaunay(xy[:, 1:])
+        n_edge = 3 * tri.npoints - 3 - len(tri.convex_hull)
+
+    print('Number of arcs per ifg: {}'.format(n_edge))
+
