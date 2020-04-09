366956e89e2fe4815d05035bf0dbb8290eff659d
ogidog <ogidog@mail.ru>
2020-04-10 01:18:15 +0700

    Home commit

--- uw_grid_wrapped.py
+++ uw_grid_wrapped.py
@@ -44,25 +44,42 @@ def uw_grid_wrapped(*args):
         print('Some phase values are zero')
 
     xy_in = args[1]
-    xy_in[:, 0] = np.array([i+1 for i in range(0, n_ps)])
+    xy_in[:, 0] = np.array([i + 1 for i in range(0, n_ps)])
 
     pix_size = args[2]
-    if pix_size==0:
-        grid_x_min=1
-        grid_y_min=1
-        n_i=max(xy_in[:,2])
-        n_j=max(xy_in[:,1])
-        grid_ij=[xy_in(:,3),xy_in(:,2)]
+    if pix_size == 0:
+        grid_x_min = 1
+        grid_y_min = 1
+        n_i = int(np.amax(xy_in[:, 2]))
+        n_j = int(np.amax(xy_in[:, 1]))
+        grid_ij = np.concatenate((xy_in[:, 2].reshape(-1, 1), xy_in[:, 1].reshape(-1, 1)), axis=1)
     else:
-        grid_x_min=min(xy_in(:,2));
-        grid_y_min=min(xy_in(:,3));
+        grid_x_min = np.amin(xy_in[:, 1])
+        grid_y_min = np.amin(xy_in[:, 2])
 
-        grid_ij(:,1)=ceil((xy_in(:,3)-grid_y_min+1e-3)/pix_size);
-        grid_ij(grid_ij(:,1)==max(grid_ij(:,1)),1)=max(grid_ij(:,1))-1;
-        grid_ij(:,2)=ceil((xy_in(:,2)-grid_x_min+1e-3)/pix_size);
-        grid_ij(grid_ij(:,2)==max(grid_ij(:,2)),2)=max(grid_ij(:,2))-1;
+        grid_ij = np.ceil((xy_in[:, 2] - grid_y_min + 0.001) / pix_size).reshape(-1, 1)
+        grid_ij[grid_ij[:, 0] == np.max(grid_ij[:, 0]), 0] = np.max(grid_ij[:, 0]) - 1
+        grid_ij = np.concatenate((grid_ij, np.ceil((xy_in[:, 1] - grid_x_min + 0.001) / pix_size).reshape(-1, 1)),
+                                 axis=1)
+        grid_ij[grid_ij[:, 1] == np.max(grid_ij[:, 1]), 1] = np.max(grid_ij[:, 1]) - 1
 
-        n_i=max(grid_ij(:,1));
-        n_j=max(grid_ij(:,2));
+        n_i = int(np.amax(grid_ij[:, 0]))
+        n_j = int(np.amax(grid_ij[:, 1]))
 
-    print()
+    ph_grid = np.zeros((n_i, n_j), dtype=int)
+    if predef_flag == 'y':
+        ph_grid_uw = np.zeros((n_i, n_j), dtype=int)
+        N_grid_uw = np.zeros((n_i, n_j), dtype=int)
+
+    prefilt_win = args[3]
+    if min(len(ph_grid), len(ph_grid[0])) < prefilt_win:
+        print('Minimum dimension of the resampled grid ({} pixels) is less than prefilter window size ({})'.format(
+            min(len(ph_grid), len(ph_grid[0])), prefilt_win))
+
+    for i1 in range(0, n_ifg):
+        if np.any(np.isreal(ph_in)):
+            ph_this = np.exp(complex(0.0, 1.0) * ph_in[:, i1])
+        else:
+            ph_this = ph_in[:, i1]
+
+        print()
