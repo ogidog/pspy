069411b8d286bc0adcdfe89332e7472a655755b4
ogidog <ogidog@mail.ru>
2020-10-15 18:04:45 +0700

    Home commit

--- clap_filt.py
+++ clap_filt.py
@@ -14,24 +14,33 @@ def clap_filt(*args):
 
     if len(args) < 2:
         alpha = 0.5
+    else:
+        alpha = args[1]
 
     if len(args) < 3:
         beta = 0.1
+    else:
+        beta = args[2]
 
     if len(args) < 4:
         n_win = 32
+    else:
+        n_win = args[3]
 
     if len(args) < 5:
         n_pad = 0
+    else:
+        n_pad = args[4]
 
     if len(args) < 6:
-        low_pass = np.zeros(n_win + n_pad)
+        low_pass = np.zeros(int(n_win) + int(n_pad))
+    else:
+        low_pass = args[5]
 
     ph = args[0]
     ph_out = np.zeros(np.shape(ph))
     [n_i, n_j] = np.shape(ph)
 
-    n_win = args[3]
     n_inc = np.floor(n_win / 4)
     n_win_i = np.ceil(n_i / n_inc) - 3
     n_win_j = np.ceil(n_j / n_inc) - 3
@@ -45,12 +54,11 @@ def clap_filt(*args):
 
     ph[np.isnan(ph)] = 0
 
-    # For gaussian std=(N-1)/(2*alpha)
+    # For gaussian std=(N-1)/(2*alpha1)
     N = 7
-    alpha = 2.5
-    std = ((N - 1) / (2 * alpha))
+    alpha1 = 2.5
+    std = ((N - 1) / (2 * alpha1))
     B = signal.gaussian(7, std=std) * signal.gaussian(7, std=std).reshape(-1, 1)
-    n_pad = args[4]
     n_win_ex = int(n_win + n_pad)
     ph_bit = np.zeros((n_win_ex, n_win_ex)).astype("complex")
 
@@ -74,9 +82,26 @@ def clap_filt(*args):
                 j1 = int(n_j - n_win + 1)
                 wf2 = np.concatenate((np.zeros((n_win, j_shift)), wf2[:, 0:n_win - j_shift]))
 
-            ph_bit[0:int(n_win), 0:int(n_win)] = ph[i1-1:i2, j1 - 1:j2]
+            ph_bit[0:int(n_win), 0:int(n_win)] = ph[i1 - 1:i2, j1 - 1:j2]
+            ph_fft = np.fft.fft2(ph_bit)
+            H = np.abs(ph_fft)
+            H = np.fft.ifftshift(signal.convolve2d(np.fft.fftshift(H), B, mode="same"))
+            meanH = np.median(H)
+            if meanH != 0:
+                H = H / meanH
+            H = H ** alpha
+            H = H - 1  # set all values under median to zero
+            H[H < 0] = 0  # set all values under median to zero
+            G = H * beta + low_pass
+            ph_filt = np.fft.ifft2(ph_fft * G)
+            ph_filt = ph_filt[0:int(n_win), 0:int(n_win)] * wf2
+            if np.isnan(ph_filt[0, 0]):
+                ph_filt[0, 0] = 0j
 
-            diff = compare_complex_objects(ph_bit, "ph_bit")
+            ph_out(i1:i2,j1:j2)=ph_out(i1:i2,j1:j2)+ph_filt;
+
+            # diff = compare_objects(G, "G")
+            diff = compare_complex_objects(ph_filt, "ph_filt")
             pass
 
     return ph_out
